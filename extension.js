d3d02cf1acecccbc7dd1aeb701ba7ccec42c0738
Henk te Sligte <henk@hjts.nl>
2015-09-13 19:35:33 +0200

    clipboard support

--- extension.js
+++ extension.js
@@ -3,11 +3,11 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 const GLib = imports.gi.GLib;
+const Gtk = imports.gi.Gtk;
 let txt_otp;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
+function _cpnow() {
+    GLib.spawn_command_line_async("/usr/bin/xclip -selection clipboard /home/henk/.otp");
 }
 
 function init() {
@@ -17,11 +17,6 @@ function init() {
                           x_fill: true,
                           y_fill: false,
                           track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
-    let txt = new St.Label({ style_class: 'googleauth-label', text: "Woot" });
-
-    button.set_child(txt);
     start_timer();
 }
 
@@ -39,6 +34,7 @@ function start_timer()
     obj = GLib.file_get_contents("/home/henk/.otp" );
     txt_otp = obj[1].toString().trim();
     let txt = new St.Label({ text: txt_otp } );
-    button.set_child(txt);    	
-    Mainloop.timeout_add_seconds(1, start_timer);
+    button.set_child(txt);
+    button.connect('button-press-event', _cpnow);    	
+    Mainloop.timeout_add_seconds(30, start_timer);
 }
